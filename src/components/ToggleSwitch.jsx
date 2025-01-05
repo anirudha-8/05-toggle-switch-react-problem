ad49cf5bc1552feca14ca90e711bdc5472ab6ecb
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-05 10:31:04 +0530

    create ToggleSwitch component

---
+++ src/components/ToggleSwitch.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const ToggleSwitch = () => {
+	const [isToggled, setIsToggled] = useState(false);
+	const handleToggle = () => {
+		setIsToggled(!isToggled);
+	};
+	return (
+		<div>
+			<input type="checkbox" name="toggle" id="toggle" />
+			<label htmlFor="toggle">{isToggled ? "On" : "Off"}</label>
+		</div>
+	);
+};
+export default ToggleSwitch;
